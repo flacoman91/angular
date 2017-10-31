c9269c03ee92f98860a35a4c7cce68527b5b6bfd
Richard Dinh <richard.dinh@ey.com>
2017-10-31 17:08:14 -0400

    3.5 SUBMISSIONS

--- app.js
+++ app.js
@@ -16,6 +16,14 @@
     };
   });
 
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+    }
+  });
+
   var gems = [
     {
       name: 'Deez Nu44ts',
