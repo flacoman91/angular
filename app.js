cbe0ac4c510170878b456d37ec62a920067f0bce
Richard Dinh <richard.dinh@ey.com>
2017-11-01 11:05:24 -0400

    4.5 directive controller. added product panels directive, moved panels controller into product panels directive

--- app.js
+++ app.js
@@ -4,18 +4,6 @@
     this.products = gems;
   });
 
-  app.controller('PanelController', function() {
-    this.tab = 1;
-
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
-
-    this.isSelected = function(checkTab) {
-      return this.tab === checkTab;
-    };
-  });
-
   app.controller('ReviewController', function() {
     this.review = {};
 
@@ -30,6 +18,25 @@
       restrict: 'A',
       templateUrl: 'product-title.html'
     };
+  });
+
+  app.directive('productPanels', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-panels.html',
+      controller: function (){
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'panels'
+    }
   });
 
   var gems = [
